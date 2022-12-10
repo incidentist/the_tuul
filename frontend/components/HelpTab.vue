42b007e6dbe27eff0ef773e606b03aaa3662c367
Dan Kurtz <andfurthermore@gmail.com>
2022-12-10 13:06:19 -0800

    Add What's New section

--- frontend/components/HelpTab.vue
+++ frontend/components/HelpTab.vue
@@ -1,41 +1,55 @@
 <template>
   <b-tab-item label="Introduction" icon="info-circle">
-    <p>
-      <strong>Step 1: Choose a music file.</strong> Click the
-      <code>Choose a song file</code> button and pick your song. Easy.
-    </p>
-    <p>
-      <strong>Step 2: Prepare the lyrics.</strong> Paste song lyrics into the
-      <code>Lyrics</code> box. Next, divide them up into screens by adding a
-      blank line between the last line of one screen and the first line of the
-      next. <strong>Pro tip:</strong> listen to the song once while reading the
-      lyrics, to make sure all the lyrics are there. Sometimes the Internet is
-      wrong.
-    </p>
-    <p>
-      <strong>Step 3: Add Timing Information.</strong> You're gonna tell The
-      Tüül when all those lyrics appear in the song. When you hit the Play
-      button, the song will start playing, and the first lyric-bit that you
-      chopped up will be highlighted in pink.
-      <strong>Hit the spacebar</strong> when that lyric comes up, and then the
-      next lyric will turn pink and await your spacebar-press.
-    </p>
-    <p>
-      If you hit the <strong>Enter</strong> key while the song is playing, that
-      will mark the end of the <strong>previous</strong> lyric. Do that when
-      there's a gap between lines or words. It can be tricky to handle both the
-      spacebar and the enter key. If the song is going too fast, slow it down by
-      clicking a different playback speed.
-    </p>
-    <p>If you need to take a break, hit the Pause button.</p>
-    <p>
-      <strong>Step 4: Create The Video.</strong> After you've given a spacebar
-      press to the last lyric in the song, click the <code>Submit</code> button.
-      Wait a few minutes -- maybe 3? Maybe 6? Eventually you'll be asked to
-      download a zip file. Inside that zip is an <code>.mp4</code> file which is
-      your karaoke video! The zip file also includes generated
-      <code>subtitles.ass</code> and <code>timings.json</code> files that were
-      used to make your video. You can ignore them.
-    </p>
+    <div class="content">
+      <p>
+        <strong>Step 1: Choose a music file.</strong> Click the
+        <code>Choose a song file</code> button and pick your song. Easy.
+      </p>
+      <p>
+        <strong>Step 2: Prepare the lyrics.</strong> Paste song lyrics into the
+        <code>Lyrics</code> box. Next, divide them up into screens by adding a
+        blank line between the last line of one screen and the first line of the
+        next. <strong>Pro tip:</strong> listen to the song once while reading
+        the lyrics, to make sure all the lyrics are there. Sometimes the
+        Internet is wrong.
+      </p>
+      <p>
+        <strong>Step 3: Add Timing Information.</strong> You're gonna tell The
+        Tüül when all those lyrics appear in the song. When you hit the Play
+        button, the song will start playing, and the first lyric-bit that you
+        chopped up will be highlighted in pink.
+        <strong>Hit the spacebar</strong> when that lyric comes up, and then the
+        next lyric will turn pink and await your spacebar-press.
+      </p>
+      <p>
+        If you hit the <strong>Enter</strong> key while the song is playing,
+        that will mark the end of the <strong>previous</strong> lyric. Do that
+        when there's a gap between lines or words. It can be tricky to handle
+        both the spacebar and the enter key. If the song is going too fast, slow
+        it down by clicking a different playback speed.
+      </p>
+      <p>If you need to take a break, hit the Pause button.</p>
+      <p>
+        <strong>Step 4: Create The Video.</strong> After you've given a spacebar
+        press to the last lyric in the song, click the
+        <code>Submit</code> button. Wait a few minutes -- maybe 3? Maybe 6?
+        Eventually you'll be asked to download a zip file. Inside that zip is an
+        <code>.mp4</code> file which is your karaoke video! The zip file also
+        includes generated <code>subtitles.ass</code> and
+        <code>timings.json</code> files that were used to make your video. You
+        can ignore them.
+      </p>
+      <h3>What's New</h3>
+      <h5>Dec 10, 2022</h5>
+      <ul>
+        <li>
+          <strong>beta.the-tuul.com</strong> - A version of The Tüül with beta
+          features is available at
+          <a href="https://beta.the-tuul.com">beta.the-tuul.com</a>. Try making
+          your song on the beta. If something doesn't work, you can go back to
+          regular ol' <a href="https://the-tuul.com">the-tuul.com</a>.
+        </li>
+      </ul>
+    </div>
   </b-tab-item>
 </template>
