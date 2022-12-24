14347ecec32126d693a8eb6b222750ea81e56d74
Dan Kurtz <andfurthermore@gmail.com>
2022-12-23 17:17:55 -0800

    Creates a browser-side video using original mp3
    Lyrics don't work because it can't find the font.

--- frontend/components/SubmitTab.vue
+++ frontend/components/SubmitTab.vue
@@ -1,16 +1,11 @@
 <template>
-  <b-tab-item
-    label="Submit"
-    icon="blender"
-    class="submit-tab"
-    :disabled="!enabled"
-  >
+  <b-tab-item label="Submit" icon="blender" class="submit-tab">
     <div class="buttons">
       <b-button
         expanded
         type="is-primary"
         :loading="isSubmitting"
-        @click="submitTimings"
+        @click="createMpeg"
         :disabled="!enabled && !isSubmitting"
       >
         Create Video
@@ -25,6 +20,8 @@
 <script>
 import { createAssFile, createScreens } from "@/lib/timing.ts";
 import { TITLE_SCREEN_DURATION } from "@/constants.js";
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+import { readFileAsync } from "@/lib/util";
 
 export default {
   props: {
@@ -89,6 +86,49 @@ export default {
       });
       this.isSubmitting = false;
       await this.saveZipFile(response);
+    },
+    async createMpeg() {
+      const songFileName = "stuff.mp3";
+      this.isSubmitting = true;
+      const ffmpeg = createFFmpeg({ log: true });
+      await ffmpeg.load();
+      // Write audio to ffmpeg-wasm's filesystem
+      await ffmpeg.FS(
+        "writeFile",
+        songFileName,
+        new Uint8Array(await readFileAsync(this.songFile))
+      );
+      // Write the subtitle font to the filesystem
+      await ffmpeg.FS(
+        "writeFile",
+        "Arial Narrow.ttf",
+        await fetchFile("http://localhost:8000/static/ArialNarrow.ttf")
+      );
+      await ffmpeg.FS("writeFile", "subtitles.ass", this.subtitles);
+      await ffmpeg.run(
+        "-f",
+        "lavfi",
+        "-i",
+        "color=c=black:s=1280x720:r=20",
+        "-i",
+        songFileName,
+        // Add subtitles
+        "-vf",
+        "ass=subtitles.ass:fontsdir=./",
+        "-shortest",
+        "-y",
+        "karaoke.mp4"
+      );
+
+      // video is a Uint8Array
+      const video = await ffmpeg.FS("readFile", "karaoke.mp4");
+      const anchor = document.createElement("a");
+      const filename = this.zipFileName;
+
+      anchor.style.display = "none";
+      anchor.href = URL.createObjectURL(new Blob([video]));
+      anchor.download = filename;
+      anchor.click();
     },
     async saveZipFile(response) {
       console.log(response);
