e41510e2a3d26cf8f179b513f88b36fa7a1e5c86
Dan Kurtz <andfurthermore@gmail.com>
2023-11-16 22:51:58 -0800

    fix: resolve timing conflicts (#24)
    When a segment start timing is earlier than the previous timing, resolve the conflict.

--- frontend/components/SongTimingTab.vue
+++ frontend/components/SongTimingTab.vue
@@ -122,67 +122,9 @@ import { KEY_CODES, LYRIC_MARKERS } from "@/constants";
 import { isMobile } from "@/lib/device";
 import LyricDisplay from "@/components/LyricDisplay.vue";
 import TimingButtons from "@/components/TimingButtons.vue";
+import TimingsList from "@/lib/TimingsList.js";
 
 interface LyricTimingEvent {}
-type LyricMarker = number;
-
-class TimingsList {
-  _timings: Array<[number, LyricMarker]>;
-  constructor() {
-    this._timings = [];
-  }
-  add(currentSegmentNum, keyCode, timestamp) {
-    if (currentSegmentNum < 0) {
-      return;
-    }
-    const marker =
-      keyCode == KEY_CODES.SPACEBAR
-        ? LYRIC_MARKERS.SEGMENT_START
-        : LYRIC_MARKERS.SEGMENT_END;
-    this._timings.push([timestamp, marker]);
-  }
-  toJson() {
-    return JSON.stringify(this._timings);
-  }
-
-  toArray() {
-    return this._timings;
-  }
-
-  timingForSegmentNum(segmentNum) {
-    const starts = this._timings.filter(
-      (t) => t[1] == LYRIC_MARKERS.SEGMENT_START
-    );
-    if (segmentNum >= starts.length) {
-      return 0;
-    }
-    return starts[segmentNum][0];
-  }
-
-  setCurrentSegment(segmentNum) {
-    // Set the segment we're currently listening for to segmentNum
-    let i = 0,
-      currentSegment = 0;
-    while (i < this._timings.length) {
-      if (this._timings[i][1] == LYRIC_MARKERS.SEGMENT_START) {
-        currentSegment += 1;
-      }
-      if (currentSegment > segmentNum) {
-        break;
-      }
-      i++;
-    }
-    this._timings = this._timings.slice(0, i);
-  }
-
-  last() {
-    return this._timings[this._timings.length - 1];
-  }
-
-  get length() {
-    return this._timings.length;
-  }
-}
 
 export default defineComponent({
   components: { LyricDisplay, TimingButtons },
