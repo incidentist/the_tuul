966374a2b255cca55f358f34a259c52ab30bd93c
Dan Kurtz <andfurthermore@gmail.com>
2023-11-19 11:11:48 -0800

    docs: update changelog

--- frontend/components/HelpTab.vue
+++ frontend/components/HelpTab.vue
@@ -30,10 +30,10 @@
       </p>
       <p>If you need to take a break, hit the Pause button.</p>
       <p>
-        <strong>Step 4: Create The Video.</strong> After you've given a spacebar
+        <strong>Step 4: Create The Video.</strong> After you've given an Enter
         press to the last lyric in the song, watch the preview and click the
-        <code>Submit</code> button. Wait a few minutes -- maybe 3? Maybe 6?
-        Eventually you'll be asked to download a zip file. Inside that zip is an
+        <code>Submit</code> button. Wait maybe 5 minutes. Eventually you'll be
+        asked to download a zip file. Inside that zip is an
         <code>.mp4</code> file which is your karaoke video! The zip file also
         includes generated <code>subtitles.ass</code> and
         <code>timings.json</code> files that were used to make your video. You
@@ -54,12 +54,20 @@
           <a href="https://the-tuul.com">the-tuul.com</a>.
         </li>
         <li>
-          If you run into problems of any kind, create a GitHub issue
+          If you run into problems, create a GitHub issue
           <a href="https://github.com/incidentist/the_tuul/issues">here</a> or
           email me at andfurthermore[AT]gmail[DOT]com.
         </li>
       </ul>
       <h3>What's New</h3>
+      <h5>November 19, 2023</h5>
+      <ul>
+        <li>
+          <strong>Fonts and Colors</strong> - You can configure the font and
+          colors of your video before you submit it. Let me know if you want a
+          specific font added.
+        </li>
+      </ul>
       <h5>October 21, 2023</h5>
       <ul>
         <li>
@@ -110,20 +118,6 @@
           slashes" mode: when you add a slash to a word to divide syllables, it
           applies that slash to every instance of the word. This speeds up the
           process of marking every syllable in a song.
-        </li>
-      </ul>
-      <h5>Dec 10, 2022</h5>
-      <ul>
-        <li>
-          <strong>beta.the-tuul.com</strong> - A version of The Tüül with beta
-          features is available at
-          <a href="https://beta.the-tuul.com">beta.the-tuul.com</a>. Try making
-          your song on the beta. If something doesn't work, you can go back to
-          regular ol' <a href="https://the-tuul.com">the-tuul.com</a>.
-        </li>
-        <li>
-          <strong>Title screens</strong> - Your videos will now have a (very
-          basic) title screen!
         </li>
       </ul>
     </div>
