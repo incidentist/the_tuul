a7ac48324df7d6f593d703ef2ef233ad738ec1ff
Dan Kurtz <andfurthermore@gmail.com>
2026-08-24 21:56:11 -0700

    0.18.0 version bump

--- frontend/components/HelpTab.vue
+++ frontend/components/HelpTab.vue
@@ -67,6 +67,10 @@
         </li>
       </ul>
       <h3>What's New</h3>
+      <h5>0.17 - August 24, 2026</h5>
+      <ul>
+        <li>Bug fixes and under-the-hood improvements.</li>
+      </ul>
       <h5>0.17 - September 19, 2025</h5>
       <ul>
         <li>Faster video creation using GPUs or Modal.com</li>
